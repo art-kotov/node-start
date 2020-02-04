fd9c1a41db58809df2cb0fa5a03074038ea2d5ed
Artem Kotov <a.kotov@nord-soft.com>
2020-02-04 13:48:36 +0300

    add server

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const {user, hello} = require("./user");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+        if (req.method === "GET") {
+            res
+                .writeHead(200, {
+                    "Content-Type": "text/html"
+                })
+                .end(`
+            <h1>Form</h1>
+            <form method="post" action="/">
+                <input name="title" type="text">
+                <button>send</button>
+            </form>
+            `)
+        } else if (req.method === "POST") {
+            let body = [];
+            req.on("data", data => {
+                body.push(Buffer.from(data));
+            });
+            req.on('end', () => {
+                const message = body.toString().split('=')[1];
+
+                res.end(`
+                    <h1>You Message: ${message}</h1>
+                `)
+            });
+        }
+    }
+);
+
+
+server.listen(3000, () => {
+    console.log("server is running...")
+});
--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -15,17 +15,52 @@ const path = require("path");
 
 let data = "console.log('Hi')";
 
-fs.writeFile(path.join(__dirname, "message.js"), data, (
+fs.writeFile(
+    path.join(__dirname, "message.js"),
+    data,
     err => {
         console.log(err);
     }
-));
+);
 
-fs.appendFile(path.join(__dirname, "message.js"), "\n123", err1 => {
-    console.log(err1)
-})
+fs.appendFile(
+    path.join(__dirname, "message.js"),
+    "\n123",
+    err => {
+        console.log(err)
+    }
+);
 
-fs.readFile(path.join(__dirname, "message.js"),'utf8', (err,data) => {
-    if (err) console.log(err);
-    console.log(data)
-})
+fs.readFile(
+    path.join(__dirname, "message.js"),
+    'utf8',
+    (err, data) => {
+        if (err) console.log(err);
+        console.log(data)
+    }
+);
+
+fs.rename(
+    path.join(__dirname, "message.js"),
+    path.join(__dirname, "rename.js"),
+    err => {
+        console.log(err)
+    }
+);
+
+fs.open(
+    path.join(__dirname, "rename.js"),
+    "r",
+    err => {
+        console.log(err)
+    }
+);
+
+// setTimeout(() => {
+//     fs.unlink(
+//         path.join(__dirname, "rename.js"),
+//         (err) => {
+//             if (err) throw err;
+//             console.log('path/file.txt was deleted');
+//         });
+// }, 5000);
