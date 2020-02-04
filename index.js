d55d387fd6d6e55a81fd8d6056e59506a3df28b3
Artem Kotov <a.kotov@nord-soft.com>
2020-02-04 14:24:10 +0300

    add files to server

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
         if (req.method === "GET") {
-            res
-                .writeHead(200, {
-                    "Content-Type": "text/html"
-                })
-                .end(`
-            <h1>Form</h1>
-            <form method="post" action="/">
-                <input name="title" type="text">
-                <button>send</button>
-            </form>
-            `)
+            res.writeHead(200, {
+                "Content-Type": "text/html; charset=utf8"
+            });
+
+            if (req.url === "/") {
+                fs.readFile(
+                    path.join(__dirname, "views", "index.html"),
+                    "utf8",
+                    (err, content) => {
+                        if (err) throw err;
+                        res.end(content)
+                    })
+            } else if (req.url === "/about") {
+                fs.readFile(
+                    path.join(__dirname, "views", "about.html"),
+                    "utf8",
+                    (err, content) => {
+                        if (err) throw err;
+                        res.end(content)
+                    })
+            } else if (req.url === "/api/users") {
+                res.writeHead(200, {
+                    "Content-Type": "text/json"
+                } )
+
+                const users = [
+                    {name: "first", age: 25},
+                    {name: "second", age: 31},
+                ]
+                res.end(JSON.stringify(users))
+
+            }
         } else if (req.method === "POST") {
             let body = [];
+
             req.on("data", data => {
                 body.push(Buffer.from(data));
             });
+
             req.on('end', () => {
                 const message = body.toString().split('=')[1];
 
@@ -25,6 +50,7 @@ const server = http.createServer((req, res) => {
                     <h1>You Message: ${message}</h1>
                 `)
             });
+
         }
     }
 );
